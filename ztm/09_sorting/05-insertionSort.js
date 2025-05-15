725241c1481185eddfaedbf68dce3efcdeb5981e
dekalouis <dekarebaudo@gmail.com>
2025-05-15 15:38:16 +0700

    learn: still at sort, doing mergesort now

--- ztm/09_sorting/05-insertionSort.js
+++ ztm/09_sorting/05-insertionSort.js
@@ -1,11 +1,53 @@
 const nums = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+// const nums = [6, 5, 3, 1, 8, 7, 2, 4];
 
 function insertionSort(arr) {
   let len = arr.length;
 
   for (let i = 0; i < len; i++) {
-    //
+    if (arr[i] < arr[0]) {
+      //if number is smaller than the first item on the array, move that number to the beginning of the list
+      /* arr.splice(i, 1) removes the current element at index i.
+    unshift(...) inserts it at the beginning.
+    This prevents unnecessary comparisons in the inner loop. */
+      arr.unshift(arr.splice(i, 1)[0]);
+    } else {
+      //otherwise find where it should go
+
+      for (let j = 1; j < i; j++) {
+        //move it to the right spot
+        /* 
+        The for loop checks where the current element should go among the already sorted items.
+        arr[i] < arr[j]: we’ve found the right spot.
+        splice(i,1) removes the element at index i.
+        splice(j,0,...) inserts it before index j.
+        The loop breaks once it inserts — ensuring the element is only moved once. */
+        if (arr[i] < arr[j]) {
+          arr.splice(j, 0, arr.splice(i, 1)[0]); // Remove arr[i] and insert at j
+          break;
+        }
+      }
+    }
   }
+  console.log(arr);
 }
 
 insertionSort(nums);
+
+/*
+we loop, outer loop goes thru array, 
+so 6, 5, 3, 1, 8, 7, 2, 4
+
+second loop, we compare 
+adjacent items, since 5 < 6, move it to the beginning
+then we compare 3 with the 6, and since 3 is smaller, move it to the beginning
+do the same for one
+then 8 stays
+then 7, is greater than 6 so move it to the right of it
+
+
+
+
+
+
+*/
---
+++ ztm/09_sorting/07-mergeSort.js
@@ -0,0 +1,27 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function mergeSort(array) {
+  if (array.length === 1) {
+    return array;
+  }
+  //split array into right and left
+  const mid = Math.floor(array.length / 2);
+  let left = array.slice(0, mid); // from index 0 up to mid (not including mid)
+  let right = array.slice(mid); // from mid to end(inc mid)
+  //   console.log(left, right);
+  //   console.log(`result of merged ${merge(mergeSort(left), mergeSort(right))}`);
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let merged = [];
+  for (let i = 0; i < left.length; i++) {}
+}
+
+const answer = mergeSort(numbers);
+console.log(answer);
+
+//divide array into 2 subsets, and divide them again until only 1 item
+//O(nlogn) because finding the index is O(1) but sorting the items will be O(n)
+//although like bubble sort we have to go through the list 1 by 1, we dont have to compare everything with everything, just the local lists with each other
+//space complexity is O(n) because need to store separate array
