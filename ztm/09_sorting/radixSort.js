25bc0df195292e8c643e87f633a279f725474dda
dekalouis <dekarebaudo@gmail.com>
2025-05-23 12:21:26 +0700

    comment: adding comment

--- ztm/09_sorting/radixSort.js
+++ ztm/09_sorting/radixSort.js
@@ -1,44 +1,45 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function radixSort(array) {
-    let maxDigitCount = mostDigits(array);
+  let maxDigitCount = mostDigits(array);
 
-    for (let k = 0; k < maxDigitCount; k++) {
-        let digitBuckets = Array.from({ length: 10 }, () => []) // [[], [], [],...]
+  for (let k = 0; k < maxDigitCount; k++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []); // [[], [], [],...]
 
-        for (let i = 0; i < array.length; i++) {
-            let digit = getDigit(array[i], k);
+    for (let i = 0; i < array.length; i++) {
+      let digit = getDigit(array[i], k);
 
-            digitBuckets[digit].push(array[i]);
-        }
-        // New order after each loop
-        array = [].concat(...digitBuckets)
+      digitBuckets[digit].push(array[i]);
     }
+    // New order after each loop
+    array = [].concat(...digitBuckets);
+  }
 
-    return array;
+  return array;
 }
 
 function getDigit(num, place) {
-    return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+  return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
 }
 
 function digitCount(num) {
-    if (num === 0) {
-        return 1;
-    }
+  if (num === 0) {
+    return 1;
+  }
 
-    return Math.floor(Math.log10(Math.abs(num))) + 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
 }
 
 function mostDigits(nums) {
-    let maxDigits = 0
+  let maxDigits = 0;
 
-    for (let i = 0; i < nums.length; i++) {
-        maxDigits = Math.max(maxDigits, digitCount(nums[i]));
-    }
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
 
-    return maxDigits;
+  return maxDigits;
 }
 
 const answer = radixSort(numbers);
 console.log(answer);
+// i need this
