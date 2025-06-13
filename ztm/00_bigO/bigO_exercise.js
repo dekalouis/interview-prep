de0bc961c9723066cbabcc4262064a1e7a5598f5
dekalouis <dekarebaudo@gmail.com>
2025-06-13 15:09:38 +0700

    adding leftover files

--- ztm/00_bigO/bigO_exercise.js
+++ ztm/00_bigO/bigO_exercise.js
@@ -1,12 +1,14 @@
 // What is the Big O of the below function? (Hint, you may want to go line by line)
 function funChallenge(input) {
-    let a = 10;
-    a = 50 + 3;
-  
-    for (let i = 0; i < input.length; i++) {
-      anotherFunction();
-      let stranger = true;
-      a++;
-    }
-    return a;
+  let a = 10; // O(1)
+  a = 50 + 3; // O(1)
+
+  for (let i = 0; i < input.length; i++) {
+    // O(n)
+    anotherFunction(); // O(n) // O(1) per iteration
+    let stranger = true; // O(n) // O(1) per iteration
+    a++; // O(n) // O(1) per iteration
   }
+  return a; // O(1)
+}
+//Big O = 6 + n
