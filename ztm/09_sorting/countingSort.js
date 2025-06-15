888e054840f32422eaa96512b5c9d8bd6ded0bde
dekalouis <dekarebaudo@gmail.com>
2025-06-15 14:12:58 +0700

    test: testing WSL on git

--- ztm/09_sorting/countingSort.js
+++ ztm/09_sorting/countingSort.js
@@ -1,27 +1,33 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function countingSort(arr, max, min) {
-    const count = [];
+  const count = [];
 
-    for (let i = min; i <= max; i++) {
-        count[i] = 0;
+  for (let i = min; i <= max; i++) {
+    count[i] = 0;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    count[arr[i]]++;
+  }
+
+  const sortedArr = [];
+
+  for (let i = min; i <= max; i++) {
+    while (count[i] > 0) {
+      sortedArr.push(i);
+      count[i]--;
     }
+  }
 
-    for (let i = 0; i < arr.length; i++) {
-        count[arr[i]]++;
-    }
-
-    const sortedArr = [];
-
-    for (let i = min; i <= max; i++) {
-        while (count[i] > 0) {
-            sortedArr.push(i);
-            count[i]--;
-        }
-    }
-
-    return sortedArr;
+  return sortedArr;
 }
 
-const answer = countingSort(numbers, Math.max(...numbers), Math.min(...numbers));
+const answer = countingSort(
+  numbers,
+  Math.max(...numbers),
+  Math.min(...numbers)
+);
 console.log(answer);
+
+//need to continue working on this
